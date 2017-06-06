72ed17df566aa17846cc3cad368878e7b0d25f9f
FranciscoRuiz <franruiz21@gmail.com>
2017-06-06 05:57:17 -0400

    implementa sgcc api para actividades

--- js/sgcc.js
+++ js/sgcc.js
@@ -1,28 +1,38 @@
-
- $.ajax({
-	dataType: 'jsonp',
+ //http://www.dotnetbull.com/2013/08/parsererror-jquery-ajax-request-jsonp.html
+//http://help.slimframework.com/discussions/questions/75-how-can-i-make-a-callback-eventjsonp-with-slim-framework
+ var home={};
+ var request;
+ if (request) {
+     request.abort();
+ }
+  
+ request = $.ajax({
+    dataType: 'jsonp',
 	url: 'http://sgcc.cl/sgccApi/public/api/actividades',
 	type: 'GET',
-	//data: {access_token: token, q: username},
-	success: function(data){
-	console.log(data);
-		$.ajax({
-			  dataType: 'jsonp',
-              url: "http://sgcc.cl/sgccApi/public/api/actividades",
-              type: "GET",
-			//data: {access_token: token, count: num_photos},
-			success: function(data2){
-			console.log(data2.data2);
-				for(x in data2.data){
-					 console.log(data2.data.fecha);
-				}
-    			},
-			error: function(data2){
-				console.log(data2);
-			}
-		});
-	},
-	error: function(data){
-		console.log(data);
-	}
-});
+	crossDomain:true,
+ });
+ request.done(function(data) {
+  //  home = JSON.parse(data);
+		  for (var key in data) {
+		   $('#listado').append('  <li class="collection-item avatar">'+
+						     ' <img src="http://sgcc.cl/baj/web/'+data[key].img +'" alt="" class="act-circle ">'+
+						      '<div class="actInfo"><h4>'+ data[key].titulo +'</h4>'+
+						     ' <p> '+ data[key].lugar+'. ' + data[key].fecha+'<br>'+
+						         data[key].descripcion +
+						     ' </p></div> '+
+					    '</li>');
+
+		}
+
+ });
+ request.fail(function(jqXHR, textStatus, errorThrown) {
+     console.log("Request failed: " + textStatus + errorThrown);
+ });
+ request.always(function(data) {
+ //	console.log(data);
+
+ });
+ 
+ 
+
